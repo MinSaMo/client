567a37831d273f8ded0f178650125be6b8fd3785
Jeon <gino9940@gmail.com>
2023-09-03 15:13:03 +0900

    feat: run functions in parallel

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import threading
+
 import websocket
 
 from detector import detector
 from speaker import speaker
 
-DEV_MODE = 0
+DEV_MODE = 1
 WS_URL = 'ws://117.16.137.205:8080/client'
 
 
@@ -14,34 +16,40 @@ class Bro:
 		self.hotword = '바로'
 		self.detector = detector.Detector()
 		self.speaker = speaker.Speaker()
-		self.ws = websocket.WebSocketApp(WS_URL, on_message=self.on_message)
+		self.ws = websocket.WebSocketApp(WS_URL, on_message=lambda ws, msg: self.on_message(ws, msg))
 
 		self.message_queue = []
+
+	def on_message(self, ws, msg):
+		print(msg)
+		# msg = json.loads(msg.decode('utf-8'))
+		pass
+
+	def run_thread(self):
 		self.ws.run_forever()
 
-	def on_message(self, msg):
-		print(msg)
+	def run_detector(self):
+		while True:
+			spoken_sentence = self.detector.detect(self.hotword)
+			print(spoken_sentence)
 
-	def handle_message_queue(self):
-		while self.message_queue:
-			message = self.message_queue.pop(0)
-			self.speaker.speak(message)
-
-	def handle_socket_message(self, message):
-		msg_type = message.get('msg_type', 0)
-		if msg_type == 2:  # Assuming priority is indicated by msg_type value
-			self.message_queue.append(message['message'])
-			self.handle_message_queue()
+	def run_speaker(self):
+		while len(self.message_queue) > 0:
+			self.speaker.ttsKR(self.message_queue[-1])
+			self.message_queue.pop()
 
 	def run(self):
-		while True:
+		socket_thread = threading.Thread(target=self.run_thread)
+		detector_thread = threading.Thread(target=self.run_detector)
+		speaker_thread = threading.Thread(target=self.run_speaker)
 
-			if len(self.message_queue) > 0:
-				pass
-			else:
-				spoken_sentence = self.detector.detect(self.hotword)
-				# send spoken_sentence via socket
-				pass
+		socket_thread.start()
+		detector_thread.start()
+		speaker_thread.start()
+
+		socket_thread.join()
+		detector_thread.join()
+		speaker_thread.join()
 
 
 if __name__ == "__main__":
