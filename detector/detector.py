1d9d95015832c1d2cdbcfdf8e067cf4709d44df9
Jeon <gino9940@gmail.com>
2023-09-13 12:12:56 +0900

    feat: response format

--- detector/detector.py
+++ detector/detector.py
@@ -1,3 +1,5 @@
+import time
+
 import speech_recognition as sr
 
 
@@ -7,8 +9,8 @@ class Detector:
 		print(sr.Microphone.list_microphone_names())
 		self.mic = sr.Microphone(device_index=2)
 
-	def detect(self, hotword: str):
-		print("Listening for hotword...")
+	def detect(self):
+		print("Listening...")
 
 		response = {
 			"success": True,
@@ -18,16 +20,15 @@ class Detector:
 
 		from main import DEV_MODE
 		if DEV_MODE == 1:
-			audio_file = sr.AudioFile('./news.wav')
-
-			with audio_file as source:
-				audio = self.r.record(source)
-
-			spoken_text = self.r.recognize_google(audio, language="ko-KR")
-
-			if hotword in spoken_text:
-				print(f"Hotword '{hotword}' detected!")
-
+			try:
+				response["transcription"] = 'REST API에 대해 설명해줘'
+				# API was unreachable or unresponsive
+				response["success"] = False
+				response["error"] = "API unavailable"
+			except sr.UnknownValueError:
+				# speech was unintelligible
+				response["error"] = "Unable to recognize speech"
+			time.sleep(10)
 		else:
 			with self.mic as source:
 				self.r.adjust_for_ambient_noise(source)
@@ -43,4 +44,4 @@ class Detector:
 				# speech was unintelligible
 				response["error"] = "Unable to recognize speech"
 
-			print(response)
+		return response
