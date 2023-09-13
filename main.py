5e38a28204941e259b45e0b6777d06316f9e0fe2
Jeon <gino9940@gmail.com>
2023-09-13 12:13:32 +0900

    feat: add socket methods

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import json
 import threading
+import time
 
 import websocket
 
@@ -7,36 +9,64 @@ from speaker import speaker
 
 DEV_MODE = 1
 WS_URL = 'ws://117.16.137.205:8080/client'
+websocket.enableTrace(True)
 
 
 class Bro:
 	def __init__(self):
-		websocket.enableTrace(True)
-
-		self.hotword = '바로'
 		self.detector = detector.Detector()
 		self.speaker = speaker.Speaker()
-		self.ws = websocket.WebSocketApp(WS_URL, on_message=lambda ws, msg: self.on_message(ws, msg))
+		self.socket = websocket.WebSocketApp(
+			url=WS_URL,
+			on_message=lambda ws, msg: self.on_message(ws, msg),
+			on_error=lambda ws, msg: self.on_error(ws, msg),
+			on_close=lambda ws: self.on_close(ws),
+			on_open=lambda ws: self.on_open(ws))
 
-		self.message_queue = []
+		self.response_queue = []
+		self.request_queue = []
 
 	def on_message(self, ws, msg):
-		print(msg)
 		# msg = json.loads(msg.decode('utf-8'))
-		pass
+		print("on_message", msg)
+
+	def on_error(self, ws, msg):
+		print("on_error", msg)
+
+	def on_close(self, ws):
+		print("on_close")
+
+	def on_open(self, ws):
+		while True:
+			if len(self.request_queue) > 0:
+				req = self.request_queue.pop()
+				ws.send(req)
 
 	def run_thread(self):
-		self.ws.run_forever()
+		self.socket.run_forever()
 
 	def run_detector(self):
 		while True:
-			spoken_sentence = self.detector.detect(self.hotword)
-			print(spoken_sentence)
+			if len(self.response_queue) == 0:
+				spoken_sentence = self.detector.detect()
+				print(spoken_sentence)
+				msg = {
+					"sender": "CLIENT",
+					"data": {
+						"script": spoken_sentence['transcription'],
+						"dialogId": 1
+					}
+				}
+				self.request_queue.append(json.dumps(msg))
+			time.sleep(1)
 
 	def run_speaker(self):
-		while len(self.message_queue) > 0:
-			self.speaker.ttsKR(self.message_queue[-1])
-			self.message_queue.pop()
+		while True:
+			if len(self.response_queue) > 0:
+				print("run_speaker", self.response_queue[-1])
+				# self.speaker.ttsKR(self.response_queue[-1])
+				self.response_queue.pop()
+			time.sleep(1)
 
 	def run(self):
 		socket_thread = threading.Thread(target=self.run_thread)
